96acfaf7311b9b15f72473bf364ed2b3f12fef2f
OE-Fernando <fernando.camacho@openenglish.com>
2024-12-11 21:52:58 +1100

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'co.peopleart.brand',
+  appName: 'brand',
+  webDir: 'www'
+};
+
+export default config;
